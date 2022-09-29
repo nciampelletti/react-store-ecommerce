4f7ef8f8284b43086523f3abcb86677646baa8e3
nciampelletti <natashat@shaw.ca>
2022-09-29 10:41:02 -0700

    getting ready for production

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,6 +1,6 @@
 import React from "react"
 // import logo from "../../assets/logo.svg"
-import logo from "../../assets/logo.svg"
+import logo from "./logo.svg"
 import { Link } from "react-router-dom"
 import { useProductsContext } from "../../context/products_context"
 import { FaTimes } from "react-icons/fa"
